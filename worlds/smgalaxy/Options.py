35db2dc1a7f476f19f9d7038f06a28ec4882afc3
Joethepic <60947591+Joethepic@users.noreply.github.com>
2022-10-25 19:41:24 -0500

    make purple coin star choice

--- worlds/smgalaxy/Options.py
+++ worlds/smgalaxy/Options.py
@@ -1,11 +1,25 @@
 import typing
-from Options import Option, DefaultOnToggle
-
-
-class EnablePurpleCoinStars(DefaultOnToggle):
+from Options import Option, Choice, Range
+ 
+ 
+class EnablePurpleCoinStars(Choice):
     """tuning this off we allow purple coin stars to count as checks do note all purple coin stars are postgame only but one."""
-    display_name = "Enable Purple Coin Stars"
-
+    display_name = "enable_purple_coin_stars"
+    option_main_game_only = 0
+    option_all = 1
+    option_none = 2
+    default = 0
+ 
+class StarstoFinish(Range):
+    "This will set the number of stars required to reach the center of the universe."
+    display_name = "Stars_to_finish"
+    range_start = 25
+    range_end = 99
+    default = 60
+ 
 smg_options: typing.Dict[str, type(Option)] = {
     "enable_purple_coin_stars": EnablePurpleCoinStars,
+    "Stars_to_finish": StarstoFinish,
 }
+
+
