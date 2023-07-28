d2e7fd87ade7d841640cbce68ef6bbd6f7df3775
Joethepic <60947591+Joethepic@users.noreply.github.com>
2023-07-28 10:04:59 -0500

    initial files 1

---
+++ worlds/smgalaxy2/Items.py
@@ -0,0 +1,9 @@
+from ...BaseClasses import Item
+
+class SMG2Item(Item):
+    game: str = "Super Mario Galaxy 2"
+
+star_item_table = {
+    "Power Star": 1304785679,
+    "Progressive Grand Star": 1304785680,
+}
---
+++ worlds/smgalaxy2/Locations.py
@@ -0,0 +1,11 @@
+from ...BaseClasses import Location
+
+class SMG2Location(Location):
+    game: str = "Super Mario Galaxy 2"
+
+# Sky Station Galaxy
+loc_SS_table = {
+    "Peewee Piranha's Temper Tantrum": 1304785679,
+    "Storming the Sky Fleet": 1304785680,
+    "Peewee Piranha's Speed Run": 1304785681,
+}
