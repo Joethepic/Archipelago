8f3078c298957d7cbfaabfd2d2c351bfabce3988
Joethepic <60947591+Joethepic@users.noreply.github.com>
2026-03-28 14:13:33 -0500

    fix accidently upstream changes

--- worlds/smgalaxy/world.py
+++ worlds/smgalaxy/world.py
@@ -150,6 +150,7 @@ class SMGWorld(World):
 
     # Output options, locations and doors for patcher
     def generate_output(self, output_directory: str):
+        self.galaxy_counts.update({"D1G1": 0})
         # Output seed name and slot number to seed RNG in randomizer client
         output_data: dict = {
             AP_WORLD_VERSION_NAME: CLIENT_VERSION,
@@ -174,6 +175,12 @@ class SMGWorld(World):
                 output_data["Options"][field.name] = list(output_data["Options"][field.name])
         output_data["Options"]["character_select"] = getattr(self.options, "character_select").value
         output_data["Options"]["mario_colors"] = getattr(self.options, "mario_colors").value
+
+        k = ["Dome 1", "Dome 2", "Dome 3", "Dome 4", "Dome 5", "Dome 6"]
+        if self.options.dome_shuffle.value:
+            self.random.shuffle(k)
+        v = [regname.TERRACE, regname.FOUNTAIN, regname.KITCHEN, regname.BEDROOM, regname.ENGINE, regname.GARDEN]
+        output_data["Options"]["dome_shuffle"] = dict(zip(k, v))
 
         # Output which item has been placed at each location
         for location in list(smgloc for smgloc in self.get_locations() if isinstance(smgloc, SMGLocation)):
