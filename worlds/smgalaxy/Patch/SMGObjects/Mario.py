566d20dd826d6b572a11468fd8f1b6ed5ab2378d
Fl-ppie <sebastiaan.aloisius@gmail.com>
2026-04-28 13:30:48 +0200

    Update Mario.py

--- worlds/smgalaxy/Patch/SMGObjects/Mario.py
+++ worlds/smgalaxy/Patch/SMGObjects/Mario.py
@@ -28,16 +28,16 @@ class MarioColours:
     SHOES_THRESHOLD: tuple = (0,0,0) # fuck you +2
 
     colour_map: dict[str, tuple] = {"Red"   : (255,  0,  0),
-                                        "Orange": (255,165,  0),
-                                        "Yellow": (255,255,  0),
-                                        "Green" : (  0,128,  0),
-                                        "Blue"  : (  0,  0,255),
-                                        "Purple": (128,  0,128),
-                                        "Black" : (  0,  0,  0),
-                                        "Brown" : (165, 42, 42),
-                                        "White" : (255,255,255),
-                                        "Pink"  : (255,192,203),
-                                        "Gray"  : (128,128,128)}
+                                    "Orange": (255,165,  0),
+                                    "Yellow": (255,255,  0),
+                                    "Green" : (  0,128,  0),
+                                    "Blue"  : (  0,  0,255),
+                                    "Purple": (128,  0,128),
+                                    "Black" : (  0,  0,  0),
+                                    "Brown" : (165, 42, 42),
+                                    "White" : (255,255,255),
+                                    "Pink"  : (255,192,203),
+                                    "Gray"  : (128,128,128)}
 
     def __init__(self, mario: RARCExtended):
         self.mario = mario
