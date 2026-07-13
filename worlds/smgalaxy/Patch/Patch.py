7dd3a715b0406991cbe6cbadf3b87c0e6d76c585
SomeJakeGuy <somejakeguy@gmail.com>
2026-07-12 20:32:17 -0400

    Found a bug related to output path not being correct.

--- worlds/smgalaxy/Patch/Patch.py
+++ worlds/smgalaxy/Patch/Patch.py
@@ -136,10 +136,11 @@ class WiiISO:
         """Takes an extracted ISO directory and re-compiles it into a playable Wii ISO."""
         output_path = Path(self.dest_path)
         output_path.parent.mkdir(parents=True, exist_ok=True)
-        
-        print("Initiating repacking ISO")
+
+        iso_path = os.path.join(self.dest_path, self.iso_name + '.iso')
+        print("Initiating repacking ISO to output: " + iso_path)
         self.calling_function = 'repack_iso'
-        rebuild_from_directory(self.temp_dir, self.dest_path + self.iso_name + '.iso', callback=self.progress_callback)
+        rebuild_from_directory(self.temp_dir, iso_path, callback=self.progress_callback)
         
         print("Repacking ISO complete")
 
