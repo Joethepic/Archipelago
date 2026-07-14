69daba9bdd8299e5c51cb28991edbf4773fcc08d
Joethepic <60947591+Joethepic@users.noreply.github.com>
2026-07-14 18:14:47 -0500

    Fix temp source fix

--- worlds/smgalaxy/SMGClient.py
+++ worlds/smgalaxy/SMGClient.py
@@ -290,7 +290,7 @@ class GalaxyContext(CommonContext):
             case "Bounced":
                 # This currently produces a client error so commenting out for now. (Deathlink should work without it)
                 #if args["source"] == self.player_names[self.slot]:
-                #    return # Don't process our own deathlink
+                return # Don't process our own deathlink
 
             case "ConnectionRefused":
                 self.dolphin_status = AP_REFUSED_STATUS
