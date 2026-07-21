b62066e89361f71226966fdb04e485b035f1aafd
Joethepic <60947591+Joethepic@users.noreply.github.com>
2026-07-21 15:48:58 -0500

    Fix path and name bugs

--- worlds/smgalaxy/Patch/Patch.py
+++ worlds/smgalaxy/Patch/Patch.py
@@ -42,9 +42,9 @@ class WiiISO:
             iso_name (str): Name of the new ISO file.
         """
         self.clean_iso_path = self.get_base_rom_path()
-        self.dest_path = os.path.dirname(patch_path)
+        self.dest_path = os.path.dirname(patch_patch)
         self.temp_dir = os.path.join(self.dest_path, "temp")
-        self.iso_name = os.path.basename(patch_path).split('.')[:-1]
+        self.iso_name = os.path.basename(patch_patch).split('.')[:-1]
         
         self.progress = None
         self.calling_function = None
@@ -134,7 +134,7 @@ class WiiISO:
         output_path = Path(self.dest_path)
         output_path.parent.mkdir(parents=True, exist_ok=True)
 
-        iso_path = os.path.join(self.dest_path, self.iso_name + '.iso')
+        iso_path = os.path.join(self.dest_path, self.iso_name[0] + '.iso')
         print("Initiating repacking ISO to output: " + iso_path)
         self.calling_function = 'repack_iso'
         rebuild_from_directory(self.temp_dir, iso_path, callback=self.progress_callback)
@@ -223,7 +223,7 @@ class GalaxyShuffle:
 
 class Patch:
     def __init__(self, patch_path: str, output: dict):
-        self.iso: WiiISO = WiiISO(patch_patch)
+        self.iso: WiiISO = WiiISO(patch_path)
 
         RARCExtended.iso_base_path = self.iso.temp_dir
         DOLExtended.iso_base_path = self.iso.temp_dir
