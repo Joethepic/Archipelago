73ff6402315f9fdde4a4c7c4eb77686f1b437576
Fl-ppie <sebastiaan.aloisius@gmail.com>
2026-09-04 01:42:12 +0200

    [Client] Fix star colour handling initialising/calculating

--- worlds/smgalaxy/SMGClient.py
+++ worlds/smgalaxy/SMGClient.py
@@ -99,22 +99,29 @@ class StarColor(NamedTuple):
 class StarColorHandler:
     pointers: dict[str, Pointer]
     star_colors: list[StarColor] = []
+    initialised: bool
 
     def __init__(self, pointers: dict[str, Pointer]):
         self.star_colors = []
         self.pointers = pointers
+        self.initialised = False
 
     def set_star_colors(self, galaxyName: str, starNum: int):
         for star in self.star_colors:
             if star.name == galaxyName + "Colours" + str(starNum):
                 star.pointer.write_value(PowerStarColorEnum.BLUE)
 
-    def set_all_star_colors(self):
+    def init_all_star_colors(self):
+        if self.initialised == True:
+            return 
+        
         self.star_colors = []
         for location in location_table.values():
             starname = self.get_pointer_name(location)
             star_color = StarColor(starname, self.pointers[starname])
             self.star_colors.append(star_color)
+        
+        self.initialised = True
 
     def get_pointer_name(self, location: SMGLocationData):
         return location.in_game_galaxy_name + "Colours" + str(location.game_address)
@@ -302,9 +309,10 @@ class GalaxyContext(CommonContext):
         if not self.needs_recalculating:
             return
         
-        for key, pointer in self.pointers.items():
+        for pointer in self.pointers.values():
             await pointer.recalculate()
-            self.starcolorhandler.set_all_star_colors()
+
+        self.starcolorhandler.init_all_star_colors()
 
         self.needs_recalculating = False
     
