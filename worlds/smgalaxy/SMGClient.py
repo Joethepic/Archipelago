97868852a3bd0ff0702edcef46b6ededcf742e2a
Fl-ppie <sebastiaan.aloisius@gmail.com>
2026-09-07 22:26:54 +0200

    [Client] Fix import issue

--- worlds/smgalaxy/SMGClient.py
+++ worlds/smgalaxy/SMGClient.py
@@ -20,7 +20,6 @@ from .regions import SMGRegionData, region_list
 from .Constants.constants import *
 from .Constants.Names.item_names import *
 from .Constants.ram_constants import *
-from StarColor import *
 import dolphin_memory_engine as dme
 
 class TypeTuple(NamedTuple):
@@ -82,6 +81,40 @@ class Pointer:
         value = struct.pack(self.value_type.value.format, value)
         dme.write_bytes(self.address, value)
 
+class StarColor(NamedTuple):
+    name: str
+    pointer: Pointer
+
+class StarColorHandler:
+    pointers: dict[str, Pointer]
+    star_colors: list[StarColor] = []
+    initialised: bool
+
+    def __init__(self, pointers: dict[str, Pointer]):
+        self.star_colors = []
+        self.pointers = pointers
+        self.initialised = False
+
+    def set_star_colors(self, galaxyName: str, starNum: int):
+        for star in self.star_colors:
+            if star.name == galaxyName + "Colours" + str(starNum):
+                star.pointer.write_value(PowerStarColorEnum.GREEN)
+
+    def init_all_star_colors(self):
+        if self.initialised == True:
+            return 
+        
+        self.star_colors = []
+        for location in location_table.values():
+            starname = self.get_pointer_name(location)
+            star_color = StarColor(starname, self.pointers[starname])
+            self.star_colors.append(star_color)
+        
+        self.initialised = True
+
+    def get_pointer_name(self, location: SMGLocationData):
+        return location.in_game_galaxy_name + "Colours" + str(location.game_address)
+  
 class GalaxyCommand(ClientCommandProcessor):
     def _cmd_dolphin(self) -> None:
         """Prints the current Dolphin status to the client."""
