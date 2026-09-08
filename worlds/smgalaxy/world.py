f87b5ca97979987f90ff461fd74417f8e526397d
BootsinSoots <theron.swanson@gmail.com>
2026-09-07 20:17:21 -0400

    [Options] Allow random colors to be chosen for Mario Colors option+2

--- worlds/smgalaxy/world.py
+++ worlds/smgalaxy/world.py
@@ -68,7 +68,7 @@ class SMGWorld(World):
                 "Gloves": self.random.choice(sorted(self.options.mario_colors.valid_values))
             }
         for key in self.options.mario_colors.valid_keys:
-            if key not in self.options.mario_colors.value.keys():
+            if key not in self.options.mario_colors.value.keys() or key is "Random":
                 self.options.mario_colors.value.update({key: self.random.choice(sorted(self.options.mario_colors.valid_values))})
 
     def create_regions(self):
